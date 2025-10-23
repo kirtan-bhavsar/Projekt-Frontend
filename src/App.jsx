8bce7c01409522e58da3b1b87f1c9f831c3acea8
kirtan_bhavsar <89853035+kirtan-bhavsar@users.noreply.github.com>
2025-10-23 11:19:46 +0530

    Final Frontend Commit (W/O Comments)

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,20 @@ import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import { Toaster } from "react-hot-toast";
   
 
-// Auth
 import LoginPage from "./pages/auth/LoginPage.jsx";
 
-// Layouts
 import AdminLayout from "./layouts/AdminLayout.jsx";
 import PMLayout from "./layouts/PmLayout.jsx";
 import DevLayout from "./layouts/DevLayout.jsx";
 
-// Admin Pages
 import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
 import ManageProjects from "./pages/admin/ManageProjects.jsx";
 import ManageUsers from "./pages/admin/ManageUsers.jsx";
 
-// Project Manager Pages
 import PMDashboard from "./pages/pm/PmDashboard.jsx";
 import MyProjects from "./pages/pm/MyProjects.jsx";
 import PMTasks from "./pages/pm/PmTasks.jsx";
 
-// Developer Pages
 import DevDashboard from "./pages/dev/DevDashboard.jsx";
 import DevTasks from "./pages/dev/DevTasks.jsx";
 
@@ -30,10 +25,9 @@ function App() {
     <Router>
       <Toaster position="top-right" />
       <Routes>
-        {/* Login Page */}
+      
         <Route path="/login" element={<LoginPage />} />
 
-        {/* ===================== ADMIN ROUTES ===================== */}
         <Route
           path="/admin"
           element={
@@ -47,7 +41,6 @@ function App() {
           <Route path="users" element={<ManageUsers />} />
         </Route>
 
-        {/* ===================== PROJECT MANAGER ROUTES ===================== */}
         <Route
           path="/pm"
           element={
@@ -61,7 +54,6 @@ function App() {
           <Route path="tasks" element={<PMTasks />} />
         </Route>
 
-        {/* ===================== DEVELOPER ROUTES ===================== */}
         <Route
           path="/developer"
           element={
@@ -74,7 +66,6 @@ function App() {
           <Route path="tasks" element={<DevTasks />} />
         </Route>
 
-        {/* Default fallback route */}
         <Route path="*" element={<LoginPage />} />
       </Routes>
     </Router>
